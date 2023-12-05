8197a5e3aa6079787a41ba983dfaa0a551a93e7e
Wonjin Park <wjpark11@gmail.com>
2023-12-05 23:13:52 +0900

    doing day01_2

--- day01_2.py
+++ day01_2.py
@@ -1,3 +1,5 @@
+import re
+
 with open("day01_input.txt", "rt") as f:
     inputs = f.readlines()
     inputs = [input.strip() for input in inputs]
@@ -15,19 +17,25 @@ DIGITS = {
 }
 
 
-def convert_to_digits(line):
-    pass
+def convert_to_digits(line: str) -> str:
+    while re.search(r"(two|one|seven|nine|three|five|eight|four|six)", line):
+        for i, _ in enumerate(line):
+            if re.match(r"(two|one|seven|nine|three|five|eight|four|six)", line[i:]):
+                digit = re.match(r"(two|one|seven|nine|three|five|eight|four|six)", line[i:]).group(1)
+                line = line.replace(digit, str(DIGITS[digit]), 1)
+                break
+    return line
 
 
 converted_inputs = []
 for line in inputs:
-    line = convert_to_digits(line)
-    converted_inputs.append(line)
+    converted_inputs.append(convert_to_digits(line))
 
-sum = 0
+calibration_value_sum = 0
 for line in converted_inputs:
     nums = [num for num in line if num in "0123456789"]
     calibration_value = int(nums[0] + nums[-1])
-    sum += calibration_value
+    calibration_value_sum += calibration_value
 
-print(sum)
+print(calibration_value_sum)
+print(convert_to_digits("twoneightwo"))
