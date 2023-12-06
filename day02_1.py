7a42263a1189fbb93c341a84c2a69a43f7e3f986
wjpark11 <wjpark11@gmail.com>
2023-12-06 17:58:22 +0900

    finIsh day02_1

--- day02_1.py
+++ day02_1.py
@@ -1,3 +1,34 @@
 with open("day02_input.txt", "rt") as f:
-    inputs = f.readlines()
-    inputs = [input.strip() for input in inputs]
+    games = f.readlines()
+    games = [game.strip() for game in games]
+
+
+def get_cube_nums(cube_str: str) -> dict:
+    cube_dict = {"red": 0, "green": 0, "blue": 0}
+    for cube in cube_str.split(", "):
+        num, color = cube.split(" ")
+        cube_dict[color] += int(num)
+
+    return cube_dict
+
+
+def is_valid_list(cube_dict_list: list) -> bool:
+    for cube_dict in cube_dict_list:
+        if cube_dict["red"] > 12:
+            return False
+        if cube_dict["green"] > 13:
+            return False
+        if cube_dict["blue"] > 14:
+            return False
+    return True
+
+
+sum = 0
+for i, game in enumerate(games):
+    cube_str = game.split(": ")[1]
+    cube_str_list = cube_str.split("; ")
+    cube_dict_list = [get_cube_nums(cube_str) for cube_str in cube_str_list]
+    if is_valid_list(cube_dict_list):
+        sum += i + 1
+
+print(sum)
