51234067787231daa074aaa06fda24c478ee4878
wjpark11 <wjpark11@gmail.com>
2023-12-08 13:54:34 +0900

    finish day04

--- day04_2.py
+++ day04_2.py
@@ -7,12 +7,23 @@ cards = [card[10:].split(" | ") for card in cards]
 cards = [
     [
         card[0].strip().replace("  ", " ").split(" "),
-        card[1].strip().replace("  ", " ").split(" ")
+        card[1].strip().replace("  ", " ").split(" "),
     ]
     for card in cards
 ]
 
 winning_card_nums = [len([num for num in card[1] if num in card[0]]) for card in cards]
 
+card_counter = {f"{i}": 0 for i in range(1, len(cards) + 1)}
 
-print(winning_card_nums)
+
+for i, card_num in enumerate(winning_card_nums):
+    for idx in range(i + 2, i + 2 + card_num):
+        card_counter[f"{idx}"] += 1
+        card_counter[f"{idx}"] += card_counter[f"{i + 1}"]
+
+sum = 0
+for _, num in card_counter.items():
+    sum += num
+
+print(sum + len(cards))
