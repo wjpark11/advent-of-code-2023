9a3b15fb0a35a4e3c809e2699bd67f5228ab0c12
Wonjin Park <wjpark11@gmail.com>
2023-12-11 00:07:02 +0900

    doing day09_1

--- day09_1.py
+++ day09_1.py
@@ -5,4 +5,5 @@ with open("day09_input.txt", "rt") as f:
 sequences = [list(map(int, line.split(" "))) for line in inputs]
 
 def get_extrapolated_value(sequence: list[int]) -> int:
+    sequences = [sequence]
     
