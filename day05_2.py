32a5fe2c0a8b96281f280ae5033a5f78cf157c94
Wonjin Park <wjpark11@gmail.com>
2023-12-10 20:27:33 +0900

    doing day05_2

--- day05_2.py
+++ day05_2.py
@@ -1,12 +1,12 @@
+from time import time
+
 with open("day05_input.txt", "rt") as f:
     inputs = f.readlines()
     inputs = [input.strip() for input in inputs]
 
 
 seed_ranges = [int(seed) for seed in inputs.pop(0).split(" ")[1:]]
-seed_ranges = [range(i, i + j) for i, j in zip(seed_ranges[::2], seed_ranges[1::2])]
-
-print(seed_ranges)
+seed_ranges = [[i, i + j - 1] for i, j in zip(seed_ranges[::2], seed_ranges[1::2])]
 
 
 maps = dict()
@@ -22,18 +22,49 @@ for line in inputs:
         maps[map_name].append([int(num) for num in line.split(" ")])
 
 
-def get_mapped_value(input_val: int, map_list: list) -> int:
+def get_mapped_range_from_single_range(input_range: list[int], map: list) -> list:
+    source_range = [map[1], map[1] + map[2] - 1]
+    mapped_range = []
+    if input_range[1] < source_range[0] or input_range[0] > source_range[1]:
+        mapped_range.append(input_range)
+        return mapped_range
+    elif input_range[0] >= source_range[0] and input_range[1] <= source_range[1]:
+        mapped_range.append([input_range[0] - map[1] + map[0], input_range[1] - map[1] + map[0]])
+        return mapped_range
+    elif input_range[0] >= source_range[0] and input_range[1] > source_range[1]:
+        mapped_range.append([input_range[0] - map[1] + map[0], source_range[1] - map[1] + map[0]])
+        mapped_range.append([source_range[1]+1, input_range[1]])
+        return mapped_range
+    elif input_range[0] < source_range[0] and input_range[1] <= source_range[1]:
+        mapped_range.append([input_range[0], source_range[0] - 1])
+        mapped_range.append([source_range[0] - map[1] + map[0], input_range[1] - map[1] + map[0]])
+        return mapped_range
+    else:
+        mapped_range.append([input_range[0], source_range[0] - 1])
+        mapped_range.append([source_range[0] - map[1] + map[0], source_range[1] - map[1] + map[0]])
+        mapped_range.append([source_range[1] + 1, input_range[1]])
+        return mapped_range
+    
+
+def get_mapped_range(input_ranges: list, map: list[int]) -> list:
+    range_list = []
+    for input_range in input_ranges:
+        range_list += get_mapped_range_from_single_range(input_range, map)
+    return range_list
+
+    
+location_list = seed_ranges
+
+for _, map_list in maps.items():
+    intermidiate_list = []
     for map in map_list:
-        if input_val in range(map[1], map[1] + map[2]):
-            return input_val - map[1] + map[0]
-    return input_val
+        intermidiate_list += get_mapped_range(location_list, map)
+    location_list = intermidiate_list
+    print(len(location_list))
+
+min_val = min(location_list, key=lambda x: x[0])[0]
+
+print(min_val)
+    
 
 
-location_nums = []
-
-for seed in seed_ranges[0]:
-    for _, map_list in maps.items():
-        seed = get_mapped_value(seed, map_list)
-    location_nums.append(seed)
-
-print(min(location_nums))
