1d0411d4e60724d39d7d592f1517373fe10d39d9
Wonjin Park <wjpark11@gmail.com>
2023-12-10 23:00:22 +0900

    finish day08_!

---
+++ day08_1.py
@@ -0,0 +1,21 @@
+with open("day08_input.txt", "rt") as f:
+    inputs = f.readlines()
+    inputs = [input.strip() for input in inputs]
+
+INSTRUCTIONS = inputs.pop(0)
+
+NODES = {line[:3] : (line[7:10], line[12:-1]) for line in inputs[1:]}
+
+steps = 0
+
+position = "AAA"
+
+while position != "ZZZ":
+    if INSTRUCTIONS[steps % len(INSTRUCTIONS)] == "R":
+        idx = 1
+    else:
+        idx = 0
+    steps += 1
+    position = NODES[position][idx]
+
+print(steps)
