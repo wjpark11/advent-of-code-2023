07d38f8dd99ac57aa0b1c65cfc657e6cace55ecd
Wonjin Park <wjpark11@gmail.com>
2023-12-10 21:12:23 +0900

    finish day06, open day07

---
+++ day06_2.py
@@ -0,0 +1,12 @@
+RACE = (54817088, 446129210351007)
+
+def get_win_number(time: int, record: int) -> int:
+    win_number = 0
+    for hold_time in range(time+1):
+        distance = (time - hold_time) * hold_time
+        if distance > record:
+            win_number += 1
+    return win_number
+
+
+print(get_win_number(*RACE))
