2da10cc208bac681efa502ae696d5a6c685288b1
Wonjin Park <wjpark11@gmail.com>
2023-12-10 23:47:10 +0900

    open day09

---
+++ day09_1.py
@@ -0,0 +1,8 @@
+with open("day09_input.txt", "rt") as f:
+    inputs = f.readlines()
+    inputs = [input.strip() for input in inputs]
+
+sequences = [list(map(int, line.split(" "))) for line in inputs]
+
+def get_extrapolated_value(sequence: list[int]) -> int:
+    
